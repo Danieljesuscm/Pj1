710f189d5384bf2e1c4d583c0c7c062c1143047d
Daniel de jesus moreira <danieljesuscmoreira@hotmail.com>
2020-09-30 00:25:57 +0000

    Resultado final

--- Usuario.cs
+++ Usuario.cs
@@ -1,20 +1,19 @@
 class usuario{
   private string nome;
-  private string avaliaçao;
-  private string nomefilme;
+  public string avaliaçao;
+  public string nomefilme;
+  
+  
 
   public void Setnome(string n){
     nome = n;
   }
-  public void Setnomefilme(string nf){
-    nomefilme = nf;
-    }
+ 
 
-  public string Setavaliaçao(string av){
-    avaliaçao = av;
-    return avaliaçao;
+ 
     
-    }
+    
+    
 
   
 }
--- main.cs
+++ main.cs
@@ -1,12 +1,33 @@
 using System;
 
+
 class MainClass {
   public static void Main (string[] args) {
+     
+           
     usuario daniel = new usuario();
     daniel.Setnome ("Daniel de Jesus");
-    daniel.Setnomefilme ("Harry Potter e o Principe Mestiço");
-    daniel.Setavaliaçao  ("filme topzera");
+    daniel.nomefilme = "Harry Potter e o Principe Misterioso";
+    daniel.avaliaçao = "filme topzera";   
+  
+  
+    
+    filme fil = new filme();
+    fil.nome = daniel.nomefilme;
+    fil.genero = "ação e aventura";
+    fil.duração = 153;
 
+
+    Console.WriteLine("nome do filme >>{0} ",daniel.nomefilme);
+    Console.WriteLine("Genero do filme>>{0} ",fil.genero);
+    Console.WriteLine("Duração do Filme >> {0} minutos",fil.duração);
+    Console.WriteLine("Avaliação do usuario >> {0} ",daniel.avaliaçao);
+
+
+    
+
+
+    
 
 
    
